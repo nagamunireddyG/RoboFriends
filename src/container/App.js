933c02c850f78b0b68451d8e90f4efd9d9bd0dd3
nagamunireddy <nag.bmsit@gmail.com>
2021-05-22 15:22:36 +0530

    Changed to react hooks

--- src/container/App.js
+++ src/container/App.js
@@ -1,47 +1,51 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ComponentError'
 import './App.css'
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+function App() {
+	// constructor() {
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// }
 
-	componentDidMount() {
+	// componentDidMount() {
+		
+	// }
+
+	const [robots, setRobots] = useState([])
+	const [ searchfield, setSearchfield ] = useState('')
+
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
-		.then(users => this.setState({robots: users}))
+		.then(users => {setRobots(users)})
+	})
+
+	const onsearchchange = (event) => {
+		setSearchfield(event.target.value)
 	}
 
-	onsearchchange = (event) => {
-		this.setState({ searchfield: event.target.value})
-	}
-
-	render () {
-		const { robots, searchfield } = this.state
-		const filteredrobots = robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-		})
-			return !robots.length ? <h1>Robots Loading</h1>
-			 : (
-				<div className= 'tc b--red bg-lightest-blue pa2'>
-					<h1 className='f2'>RoboFriends</h1>
-					<SearchBox searchchange= {this.onsearchchange}/>
-					<Scroll>
-						<ErrorBoundary>
-							<CardList robots={filteredrobots}/>
-						</ErrorBoundary>
-					</Scroll>
-				</div>
-			);
-	}
+	const filteredrobots = robots.filter(robot =>{
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+	})
+	return !robots.length ? <h1>Robots Loading</h1>
+	 : (
+		<div className= 'tc b--red bg-lightest-blue pa2'>
+			<h1 className='f2'>RoboFriends</h1>
+			<SearchBox searchchange= {onsearchchange}/>
+			<Scroll>
+				<ErrorBoundary>
+					<CardList robots={filteredrobots}/>
+				</ErrorBoundary>
+			</Scroll>
+		</div>
+	);	
 }
 
 export default App;
